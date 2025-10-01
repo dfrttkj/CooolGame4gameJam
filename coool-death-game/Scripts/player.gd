9766047a07b6ac4580396bdcf2a53a22706fb207
Zaki Abou Ayoub <zaki.abouayoub@edu.htl-klu.at>
2025-10-01 23:00:33 +0200

    finished camera for the most part

--- coool-death-game/Scripts/player.gd
+++ coool-death-game/Scripts/player.gd
@@ -8,28 +8,35 @@ extends CharacterBody3D
 @export var jump_power = 80
 
 #Vars for Camera movement
+@export var mouseSensitivity = 0.01
 @onready var neck := $Pivot/neck
 @onready var camera := $Pivot/neck/Camera3D
+@onready var character := $"Pivot/character-d2"
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
+func _unhandled_input(event: InputEvent) -> void:								#For camera movement
+	if event is InputEventMouseButton:											#if mouse click, mouse cursor gone
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event.is_action("ui_cancel"):											#clicking escape will make your cursor come back with milk
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:							#everything important for camera movement
+		if event is InputEventMouseMotion:
+			var relative = event.relative * mouseSensitivity
+			character.rotate_y(-relative.x)										#rotate character skin with the camera, just on the y axis
+			neck.rotate_y(-relative.x)
+			camera.rotate_x(-relative.y)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(90))				#clamps the x rotation of your head so you cant break ya neck
+	
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	
 	if Input.is_action_just_pressed("jump"):
 		target_velocity.y += 80
 
