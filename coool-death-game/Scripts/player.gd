94cfc795b08632f1f8def5cb9de34cc6c639abdf
Zaki Abou Ayoub <zaki.abouayoub@edu.htl-klu.at>
2025-10-01 22:07:01 +0200

    finished player movement for now, doing camera movement next

--- coool-death-game/Scripts/player.gd
+++ coool-death-game/Scripts/player.gd
@@ -4,9 +4,20 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+# How high the player can jump
+@export var jump_power = 80
+
+#Vars for Camera movement
+@onready var neck := $Pivot/neck
+@onready var camera := $Pivot/neck/Camera3D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 var target_velocity = Vector3.ZERO
-
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -19,11 +30,13 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+	if Input.is_action_just_pressed("jump"):
+		target_velocity.y += 80
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
-		$Pivot.basis = Basis.looking_at(direction)
+		#$Pivot.basis = Basis.looking_at(direction)
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
