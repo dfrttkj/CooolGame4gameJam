beab0ffabb48e6924201a0bd05acc9fc68fe01d2
ramsak <ramsakj@edu.htl-villach.at>
2025-10-02 16:41:31 +0200

    fixed jumping

--- coool-death-game/Scripts/player.gd
+++ coool-death-game/Scripts/player.gd
@@ -29,30 +29,29 @@ func _unhandled_input(event: InputEvent) -> void:								#For camera movement
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
 	
-	if Input.is_action_just_pressed("jump"):
-		target_velocity.y += 80
-
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		#$Pivot.basis = Basis.looking_at(direction)
-
+	
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
+	
+	# Vertical velocity (jumping and gravity)
+	if is_on_floor():
+		# Reset vertical velocity when on floor to prevent accumulation
+		target_velocity.y = 0
+		# Jump only when on floor
+		if Input.is_action_just_pressed("jump"):
+			target_velocity.y += jump_power
+	else:
+		# Apply gravity when in air
+		target_velocity.y = velocity.y - (fall_acceleration * delta)
+	
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
